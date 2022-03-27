1b3c01103cbc985868ee1cb42077b2e09b921e3c
shou-watanabe <watanabeshou1520@gmail.com>
2022-03-27 18:12:22 +0900

    :sparkles: add list struct

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: shwatana <shwatana@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/26 10:32:35 by shwatana          #+#    #+#             */
-/*   Updated: 2022/03/26 11:13:46 by shwatana         ###   ########.fr       */
+/*   Updated: 2022/03/27 10:54:41 by shwatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,14 @@
 # include <errno.h>
 
 # define BUFFER_SIZE 32
+
+typedef struct s_list
+{
+	int				fd;
+	char			*text;
+	struct s_list	*prev;
+	struct s_list	*next;
+}	t_list;
 
 char	*get_next_line(int fd);
 
