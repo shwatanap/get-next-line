ebd43ebb6278738c6c75d20c2346bd58e1d15cce
shou-watanabe <watanabeshou1520@gmail.com>
2022-03-27 18:15:34 +0900

    :sparkles: add base function

---
+++ get_next_line_utils.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shwatana <shwatana@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/26 10:32:35 by shwatana          #+#    #+#             */
+/*   Updated: 2022/03/27 18:14:54 by shwatana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_UTILS_H
+# define GET_NEXT_LINE_UTILS_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdint.h>
+# include <errno.h>
+
+# define BUFFER_SIZE 32
+
+typedef struct s_list
+{
+	int				fd;
+	char			*text;
+	struct s_list	*prev;
+	struct s_list	*next;
+}	t_list;
+
+#endif
