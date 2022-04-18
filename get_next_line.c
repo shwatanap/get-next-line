4236bf69e8b713a64fb866af5758f115e5b3ba37
shou-watanabe <watanabeshou1520@gmail.com>
2022-04-18 21:45:50 +0900

    :wip: wip

--- get_next_line.c
+++ get_next_line.c
@@ -6,97 +6,112 @@
 /*   By: shwatana <shwatana@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/26 10:36:52 by shwatana          #+#    #+#             */
-/*   Updated: 2022/03/29 22:56:15 by shwatana         ###   ########.fr       */
+/*   Updated: 2022/04/18 18:22:29 by shwatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static t_list	*new_str_list(int fd);
-static t_list	*find_list_by_fd(int fd, t_list *str_list);
-static bool		read_buf(t_list *str_list);
+static char	*read_buf(char *str, int fd);
+static char	*format_line(char *strage);
+static char	*ft_strage(char *strage);
 
 char	*get_next_line(int fd)
 {
-	int		n;
-	char	*buf;
-	char	*line;
-	bool	is_read;
-	t_list	*target;
-	t_list	*str_list;
+	char		*line;
+	static char	*strage;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	if (str_list == NULL)
-	{
-		str_list = new_str_list(fd);
-		if (str_list == NULL)
-			return (NULL);
-	}
-	target = find_list_by_fd(fd, str_list);
-	is_read = read_buf(target);
-	while (target != NULL)
-	{
-		printf("%s\n", target->text);
-		target = target->next;
-	}
+	strage = read_buf(strage, fd);
+	if (strage == NULL)
+		return (NULL);
+	// line = format_line(line, strage);
+	// printf("======\n");
+	// printf("%s\n", format_line(line, strage));
+	// printf("======\n");
+	line = format_line(strage);
+	strage = ft_strage(strage);
 	return (line);
 }
 
-static t_list	*find_list_by_fd(int fd, t_list *str_list)
+static char	*format_line(char *strage)
 {
-	t_list	*new_list;
+	size_t	i;
+	char	*line;
 
-	while (str_list == NULL)
+	i = 0;
+	if (strage == NULL)
+		return (NULL);
+	while (strage[i] == '\n' || strage[i] == '\0')
+		i++;
+	line = malloc(sizeof(char) * (i + 1 + (strage[i] == '\n')));
+	if (line == NULL)
+		return (NULL);
+	i = 0;
+	while (strage[i] && strage[i] != '\n')
 	{
-		if (str_list->fd == fd)
-			return (str_list);
-		str_list = str_list->prev;
+		line[i] = strage[i];
+		i++;
 	}
-	new_list = new_str_list(fd);
-	if (new_list == NULL)
-		return (NULL);
-	return (new_list);
+	if (strage[i] == '\n')
+	{
+		line[i] = strage[i];
+		i++;
+	}
+	line[i] = '\0';
+	return (line);
 }
 
-static t_list	*new_str_list(int fd)
+char	*ft_strage(char *strage)
 {
-	t_list	*new_list;
+	size_t	size;
+	int		c;
+	char	*new_strage;
 
-	new_list = (t_list *)malloc(sizeof(t_list));
-	if (new_list == NULL)
+	size = 0;
+	while (strage[size] && strage[size] != '\n')
+		size++;
+	if (strage[size] == '\0')
+	{
+		free(strage);
 		return (NULL);
-	new_list->fd = fd;
-	new_list->text = NULL;
-	new_list->prev = NULL;
-	new_list->next = NULL;
-	return (new_list);
+	}
+	new_strage = (char *)malloc(sizeof(char) * (ft_strlen(strage) - size + 1));
+	if (!new_strage)
+		return (NULL);
+	size++;
+	c = 0;
+	while (strage[size])
+		new_strage[c++] = strage[size++];
+	new_strage[c] = '\0';
+	free(strage);
+	return (new_strage);
 }
 
-static bool	read_buf(t_list *str_list)
+static char	*read_buf(char *strage, int fd)
 {
 	char	*buf;
-	int		read_size;
+	size_t	read_size;
 
-	if (str_list == NULL)
-		return (NULL);
 	while (true)
 	{
 		buf = (char *)malloc(sizeof(char) * ((size_t)BUFFER_SIZE + 1));
 		if (buf == NULL)
 			return (NULL);
-		read_size = read(str_list->fd, buf, BUFFER_SIZE);
+		read_size = read(fd, buf, BUFFER_SIZE);
+		// printf("read_size: %d\n", read_size);
 		if (read_size <= 0)
 		{
 			free(buf);
 			if (read_size == 0)
-				return (true);
-			return (false);
+				return (strage);
+			return (NULL);
 		}
 		buf[read_size] = '\0';
-		str_list->text = ft_strjoin(str_list->text, buf);
+		strage = ft_strjoin(strage, buf);
 		free(buf);
-		if (str_list->text == NULL)
-			return (false);
+		if (strage == NULL)
+			return (NULL);
 	}
 }
