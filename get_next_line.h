01a0301096fbfc65e2bb4a4879578a0a90f96050
shou-watanabe <watanabeshou1520@gmail.com>
2022-04-20 18:37:32 +0900

    :wastebasket: unused struct

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: shwatana <shwatana@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/26 10:32:35 by shwatana          #+#    #+#             */
-/*   Updated: 2022/03/29 10:18:59 by shwatana         ###   ########.fr       */
+/*   Updated: 2022/04/20 18:37:09 by shwatana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,9 @@
 # include <unistd.h>
 # include <fcntl.h>
 # include <stdint.h>
-# include <errno.h>
 # include <stdbool.h>
 
 # define BUFFER_SIZE 32
-
-typedef struct s_list
-{
-	int				fd;
-	char			*text;
-	struct s_list	*prev;
-	struct s_list	*next;
-}	t_list;
 
 char	*get_next_line(int fd);
 char	*ft_strjoin(const char *s1, const char *s2);
